7d2e78ec5531e5f1f0107a0a978b1da6629cd205
Patrick amusivwa <patrick.amusivwa@students.jkuat.ac.ke>
2022-09-29 09:36:30 +0300

    initialize variables

---
+++ game.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+int secretnumber = 7;
+int guess;
+while(secretnumber!=guess){
+cout<<"Guess the number: ";
+cin>>guess;
+
+
+
+}
+
+
+
+
+    return 0;
+
+}
